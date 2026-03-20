3265d1af64ac806fe8ba839316a7cfd917071dc7
myshchyshyn9898-bit <myshchyshyn9898@gmail.com>
2026-03-20 22:57:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." # Твій повний
 
 # Посилання на твій бот (заміни на username свого бота)
 BOT_USERNAME = "YourBotUsername"
+# ОСЬ ЦЕЙ РЯДОК ТРЕБА ДОДАТИ:
+BASE_URL = "https://твій-нік.github.io/delivery-saas/"
 # config.py
 SUPER_ADMIN_IDS = [6889016268] # Твій ID та ID помічника
