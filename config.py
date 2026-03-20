84428d3a7702ef1d4c4d8ff8f7b726addae09353
myshchyshyn9898-bit <myshchyshyn9898@gmail.com>
2026-03-20 23:11:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_TOKEN = '8675947904:AAEaCGrJ8uWH79uJ5GsBqGwOLbSlSGDpobg'
 SUPABASE_URL = "https://kvanzkcwpwmfexsmldvx.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." # Твій повний ключ
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2YW56a2N3cHdtZmV4c21sZHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzgyMzksImV4cCI6MjA4OTYxNDIzOX0.ZHXB9-PwJhH07LzPGpxK0HD-BkLGlf5w2L4WbgrX4JA" # Твій повний ключ
 
 # Посилання на твій бот (заміни на username свого бота)
 BOT_USERNAME = "YourBotUsername"
