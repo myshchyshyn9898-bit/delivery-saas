a7dd846c60f8270c251c899dd1a7adbc7b4d3e32
myshchyshyn9898-bit <myshchyshyn9898@gmail.com>
2026-03-24 00:23:24 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,12 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
-import time  # <-- ДОДАЛИ ДЛЯ ГЕНЕРАЦІЇ УНІКАЛЬНОГО ЧАСУ
+import time
 
 # Головне посилання на твій GitHub Pages
 URL = "https://myshchyshyn9898-bit.github.io/delivery-saas/"
 
-# --- 1. МЕНЮ РЕЄСТРАЦІЇ (Для нових юзерів) ---
-# Ця кнопка запускає наш новий крутий додаток!
+# --- 1. МЕНЮ РЕЄСТРАЦІЇ ---
 reg_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="🚀 Відкрити DelivePro", web_app=WebAppInfo(url=f"{URL}delivepro.html"))]
@@ -16,11 +15,11 @@ reg_kb = ReplyKeyboardMarkup(
 
 # --- 2. МЕНЮ ВЛАСНИКА ---
 def get_owner_kb(biz_id):
-    t = int(time.time()) # Генеруємо унікальне число (секунди)
+    t = int(time.time())
     return ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="📊 Дашборд", web_app=WebAppInfo(url=f"{URL}dashboard.html?biz_id={biz_id}&v={t}"))],
-            [KeyboardButton(text="📊 Зробити звіт")], # <-- ДОДАВ КОМУ
+            [KeyboardButton(text="📊 Зробити звіт")],
             [KeyboardButton(text="⚙️ Налаштування бізнесу"), KeyboardButton(text="👥 Персонал")]
         ],
         resize_keyboard=True
@@ -28,22 +27,22 @@ def get_owner_kb(biz_id):
 
 # --- 3. МЕНЮ МЕНЕДЖЕРА ---
 def get_manager_kb(biz_id):
-    t = int(time.time()) # Кеш більше не пройде!
+    t = int(time.time())
     return ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="📝 Нове замовлення", web_app=WebAppInfo(url=f"{URL}form.html?biz_id={biz_id}&v={t}"))],
-            [KeyboardButton(text="📊 Зробити звіт")], # <-- ДОДАВ КОМУ
-            [KeyboardButton(text="📂 Активні замовлення", web_app=WebAppInfo(url=f"{URL}orders.html?biz_id={biz_id}&v={t}"))] # ЗМІНИЛИ НА orders.html
+            [KeyboardButton(text="📊 Зробити звіт")],
+            [KeyboardButton(text="📂 Активні замовлення", web_app=WebAppInfo(url=f"{URL}orders.html?biz_id={biz_id}&v={t}"))]
         ],
         resize_keyboard=True
     )
 
 # --- 4. МЕНЮ КУР'ЄРА ---
 def get_courier_kb(biz_id):
-    t = int(time.time()) # Кеш більше не пройде!
+    t = int(time.time())
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📦 Мої доставки", web_app=WebAppInfo(url=f"{URL}orders.html?biz_id={biz_id}&v={t}"))] # ЗМІНИЛИ НА orders.html
+            [KeyboardButton(text="📦 Мої доставки", web_app=WebAppInfo(url=f"{URL}orders.html?biz_id={biz_id}&v={t}"))]
         ],
         resize_keyboard=True
     )
