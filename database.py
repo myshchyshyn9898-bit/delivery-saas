61ef876e141d64a47752324837e39c293345cc2c
myshchyshyn9898-bit <myshchyshyn9898@gmail.com>
2026-03-25 12:42:09 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import datetime
 from supabase import create_client, Client
+# Імпортуємо вже готові змінні, які config.py дістав із системних змінних Railway
+from config import SUPABASE_URL, SUPABASE_KEY 
 
-# Дані беремо з налаштувань
-SUPABASE_URL = "https://kvanzkcwpwmfexsmldvx.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2YW56a2N3cHdtZmV4c21sZHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzgyMzksImV4cCI6MjA4OTYxNDIzOX0.ZHXB9-PwJhH07LzPGpxK0HD-BkLGlf5w2L4WbgrX4JA"
-
+# Тепер ініціалізуємо клієнта, використовуючи імпортовані дані
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+# Далі твій код без змін...
 
 # ==========================================
 # ФУНКЦІЇ ДЛЯ БІЗНЕСУ ТА АДМІНІСТРУВАННЯ
